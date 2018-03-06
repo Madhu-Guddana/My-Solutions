91a8d2374c2b37f7a869fec93dfb8934ec04ad6d
Madhu-Guddana <36655676+Madhu-Guddana@users.noreply.github.com>
2018-03-06 22:18:46 +0530

    Create count_luck.py

---
+++ back_tracking/count_luck.py
@@ -0,0 +1,61 @@
+# Source: https://www.hackerrank.com/challenges/count-luck/problem
+
+#!/bin/python3
+
+import sys
+
+def get_available_paths(matrix, pos):
+  r = pos[0]
+  c = pos[1]
+  neigh = [            (r-1, c),
+           (r,   c-1),           (r,   c+1),
+                       (r+1, c)]
+  ret_val = []
+  for ele in neigh:
+    i,j = ele[0], ele[1]
+    if 0<=i<len(matrix) and 0<=j<len(matrix[i]):
+      if matrix[i][j]== '.' or matrix[i][j]== '*':
+        ret_val.append((i,j))
+  return ret_val
+
+
+def traverse(matrix, pos, count):
+  if matrix[pos[0]][pos[1]] == '*':
+    return count
+  matrix[pos[0]][pos[1]] = 'V' # Visited
+  paths = get_available_paths(matrix, pos)
+
+  if len(paths) > 1:
+    count+=1
+  for path in paths:
+    ret_val = traverse(matrix, path, count)
+    if ret_val!= -1:
+      return  ret_val
+  return -1
+
+def countLuck(matrix, k):
+  source =[(i,j) for i in range(len(matrix)) for j in range(len(matrix[i])) if matrix[i][j] == 'M']
+  source = source[0]
+  ans = traverse(matrix, source, 0)
+  if ans == k:
+    return 'Impressed'
+  else:
+    return 'Oops!'
+
+if __name__ == "__main__":
+    t = int(raw_input().strip())
+    for a0 in range(t):
+        n, m = raw_input().strip().split(' ')
+        n, m = [int(n), int(m)]
+        matrix = []
+        matrix_i = 0
+        for matrix_i in range(n):
+           matrix_t = str(raw_input()).strip()
+           matrix.append([char for char in matrix_t])
+        k = int(raw_input())
+        result = countLuck(matrix, k)
+        print(result)
+
+
+
+
