3fc4d492cb461115f1655e000a58ab76f137fbc3
Madhu-Guddana <36655676+Madhu-Guddana@users.noreply.github.com>
2018-03-18 10:50:42 +0530

    Update closest_palindrome.py

--- adhoc/closest_palindrome.py
+++ adhoc/closest_palindrome.py
@@ -1,4 +1,11 @@
 # Question: https://practice.geeksforgeeks.org/problems/closest-palindrome/0
+# Given a number, find next near by palindrome.
+# Eg: given 98 provide 99
+# Given 90, provide 88.
+# If number itself is a palindrome, give its neighbour palindrome
+# If 101 give back 99
+# If there are 2 closes palindrome, then give lower number amoung them
+# If 10 is given, both 9 and 11 are nearby palindrome, then give 9 as answer.
 # Algorithm: https://www.geeksforgeeks.org/given-a-number-find-next-smallest-palindrome-larger-than-this-number/
 
 def is_palindrom(arr):
@@ -26,8 +33,9 @@ def num_to_arr(num):
   return arr[::-1]
 
 def append_mirror_image(arr, include_mid):
-  # Generates the palindrome, by mirroring the existing arr.
+  # Generates the palindrome, by mirroring and appending the existing arr.
   # length of result must be odd or even will be decided by include_mid variable
+  # Eg: given [1,2,3] we return [1,2,3,3,2,1] if include_mid is true, else we return [1,2,3,2,1]
   i = len(arr)-1
   if not include_mid:
     i-=1
@@ -38,6 +46,8 @@ def append_mirror_image(arr, include_mid):
 
 def provide_half_array(arr):
   # Simply provide half of the element, in case of odd number this include mid element as well
+  # Eg: [1,2,3,4] will return [1,2]
+  # [1,2,3,4,5] will return [1,2,3]
   temp_len = len(arr)
   if temp_len%2 == 1:
     temp_len = temp_len+1
@@ -46,6 +56,14 @@ def provide_half_array(arr):
 
 def get_lower_palindrome(pal_arr):
   # Provides a num, that is lesser than input and is palindrome.
+  # Idea is- Decrease the mid element by 1, that must be enough.
+  # But if mid element is 0, we need to propogate the substraction to its right, like how we do in normal substraction.
+  # So the best way is:
+  # 1) Bisect the array
+  # 2) Convert it to num
+  # 3) Substract it by 1
+  # 4) Convert back to num
+  # 5) Mirror the answer to make it reflective to the right part of the palindrome.
   
   # Special Case.
   if pal_arr == [1,1]:
@@ -68,7 +86,9 @@ def get_lower_palindrome(pal_arr):
   return arr_to_num(arr)
 
 def get_higher_palindrome(pal_arr):
-  # Provides a num, that is greater than input and is palindrome.
+  # This is similar to get_lower_palindrome.
+  # In fact we can avoid these code redundancy in production code.
+  # For Practice and competition, this much is enough.
   
   # Increase number from mid.
   arr = provide_half_array(pal_arr)
@@ -92,10 +112,13 @@ def solve(num):
   if num<10:
       return num-1
   arr = num_to_arr(num)
-#   if is_palindrom(arr):
-#     return num
+  
+  # In case, we consider only nearest palindrome, we must uncomment following lines.
+  #   if is_palindrom(arr):
+  #     return num
     
   
+  # i and j will point to characters that are mirror image(or altleast suppose to be mirror image).
   i = len(arr)//2
   if len(arr)%2 == 1:
     j = i+1
@@ -103,30 +126,33 @@ def solve(num):
     j = i
   i = i-1
 
+  # smaller indicates, if palindrome we got, is less than the num or not.
   smaller = None
   while(i>=0 and j<len(arr)):
     if arr[i] == arr[j]:
       i = i-1
       j = j+1
       continue
-
+    arr[j] = arr[i]
+    
+    # Update smaller, only 1 time.
     if smaller == None:
       smaller = bool(arr[i] < arr[j])
-    arr[j] = arr[i]
-
-
+ 
   if smaller == True:
     lower_palindrom = arr_to_num(arr)
     higher_palindrome = get_higher_palindrome(arr)
   elif smaller==False:
     lower_palindrom,  = get_lower_palindrome(arr),
     higher_palindrome = arr_to_num(arr)
-  else:
+  else: # Case where number itself is palindrome.
     lower_palindrom  = get_lower_palindrome(arr)
     higher_palindrome = get_higher_palindrome(arr)
 
   return lower_palindrom if num-lower_palindrom <= higher_palindrome-num else higher_palindrome
 
+
+#### Entry point for the programme ####
 for _ in range(int(input())):
   num = int(input())
   print(solve(num))
