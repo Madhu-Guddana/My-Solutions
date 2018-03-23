4095c5b2bcab5a6a7240b84a39552f123d369137
Madhu-Guddana <36655676+Madhu-Guddana@users.noreply.github.com>
2018-03-23 11:40:09 +0530

    Update max_sum_rectangle.py

--- dynamic_programming/max_sum_rectangle.py
+++ dynamic_programming/max_sum_rectangle.py
@@ -1,6 +1,6 @@
 # Question:
 # Given a 2D array, find the maximum sum subarray in it
-# Source: 
+# Source:
 # 1) https://practice.geeksforgeeks.org/problems/maximum-sum-rectangle/0
 # 2) https://www.youtube.com/watch?v=yCQN096CwWM
 # 3) https://www.hackerrank.com/contests/pscg-coding-contest-march-2016/challenges/maximum-sub-matrix
@@ -15,7 +15,7 @@ def kadane(arr):
       max_sofar = temp_max
       high = i
       low = temp_low
-      
+
 
     if temp_max < 0:
       temp_max=0
@@ -28,9 +28,16 @@ def max_rect_sum(arr):
   max_sum = arr[0][0]
   l = r = t = d = 0 # left right top down
   for i in range(len(arr)): # Consider each row, check if max_array start from here.
+    # temp_arr used for incremental sum.
     temp_arr = [0]*len(arr[0])
-    for j in range(i, len(arr)): # Find max_arr possible with i as starting index.
-      temp_arr = [temp_arr[k]+ arr[j][k] for k in range(len(arr[j]))]
+    
+    for j in range(i, len(arr)): # Find max_arr possible with row starting i and ending with j.
+
+      # Array under consideration is arr[i:j+1]
+      # Add the correcponding element of jth row elements to temp_arr.
+      # Since incremental sum, this will be equal to adding each elements of a col in arr[i:j+1]
+      temp_arr = [temp_arr[k]+ arr[j][k] for k in range(len(arr[j]))] # Convert 2D to 1D matrix by sum.
+
       temp_sum, left, right = kadane(temp_arr)
       if temp_sum > max_sum:
         l,r,t,d = left, right, i, j
@@ -38,15 +45,11 @@ def max_rect_sum(arr):
 
   return max_sum
 
-
-for _ in range(int(input())):
-  n, m = input().split(' ')
-  n, m = int(n), int(m)
-  temp_arr = input().split(' ')
-  arr = []
-  for i in range(n):
-    a = []
-    for j in range(m):
-      a.append(int(temp_arr[i*m+j]))
-    arr.append(a)
-  print (max_rect_sum(arr))
+arr = [
+  [1,  2, -1,-4,-20],
+  [-8,-3,  4, 2,  1],
+  [3,  8, 10, 1,  3],
+  [-4,-1,  1, 7, -6]
+]
+print (max_rect_sum(arr))
+# Expected Answer: 29
