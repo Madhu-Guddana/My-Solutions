4da8549205cb6beeada18a8725072b9df5c0daed
Madhu-Guddana <36655676+Madhu-Guddana@users.noreply.github.com>
2018-03-23 09:00:43 +0530

    Update wildcharacter.py

--- adhoc/wildcharacter.py
+++ adhoc/wildcharacter.py
@@ -3,32 +3,42 @@
 # The wildcard pattern can include the characters ‘?’ and ‘*’
 # ‘?’ – matches any single character
 # ‘*’ – Matches any sequence of characters (including the empty sequence)
+# Print True if pattern matches the string, False otherwise.
 
 # Sources:
 # 1. https://practice.geeksforgeeks.org/problems/wildcard-pattern-matching/1
 # 2. https://www.hackerrank.com/contests/placement-preparation-1/challenges/string-matching-with-wildcards/problem
 
+# The recursive Program is self explanatory.
+
 pattern = str(raw_input())
 string = str(raw_input())
 pattern = pattern.strip()
 string = string.strip()
-def wildCard(str1, str2):
-    if not str1 and not str2:
+def wildCard(pattern, string):
+    
+    # Case where both are empty.
+    if not pattern and not string:
         return True
-    if not str2:
-        for char in str1:
+    
+    # Case where String is empty, then pattern must be full of '*' char.
+    if not string:
+        for char in pattern:
             if char != "*":
                 return False
         return True
-    if not str1:
+    
+    # Case where string is not empty but pattern is empty.
+    if not pattern:
         return False
     
-    if str1[0] == "?":
-        return wildCard(str1[1:], str2[1:])
-    elif str1[0] == "*":
-        return wildCard(str1[1:], str2) or wildCard(str1[1:], str2[1:]) or wildCard(str1, str2[1:])
+    if pattern[0] == "?":
+        return wildCard(pattern[1:], string[1:])
+    elif pattern[0] == "*":
+        # Case          * consumes 0 char                * consumes 1 char              * consumes more than 1 char.
+        return wildCard(pattern[1:], string) or wildCard(pattern[1:], string[1:]) or wildCard(pattern, string[1:])
     else :
-        return_val = str1[0]==str2[0] and wildCard(str1[1:], str2[1:])
+        return_val = pattern[0] == string[0] and wildCard(pattern[1:], string[1:])
         return return_val
  
 print str(wildCard(pattern, string)).upper()
