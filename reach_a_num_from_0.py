5a7ad38fe0278786ca87a9d840e97320bb577cb0
Madhu-Guddana <36655676+Madhu-Guddana@users.noreply.github.com>
2018-03-23 13:07:13 +0530

    Update reach_a_num_from_0.py

--- reach_a_num_from_0.py
+++ reach_a_num_from_0.py
@@ -1,6 +1,5 @@
 # Question:
-#   Given a number, reach the number from 0 by performing any of the 3
-#   operation.
+#   Given a number, reach the number from 0 by performing any of the 3 operations.
 #   1) Addition of 1
 #   2) Subtracting 1
 #   3) Multiplication of 2
@@ -12,8 +11,8 @@
 #
 #   Find the minimum cost required to reach the number from 0.
 
-# Solution:
-# 1.When it is even, then only option we have to reach num/2 is sub and
+# Solution: we go from number to 0 instead.
+# 1.When the num is even, then only option we have to reach num/2 is sub and
 # div. Check for cost of both and do accordingly.
 # 2. When it is odd, then we have 2 situation,
 #   a. num+1 and num-1 are even, but which one will have more factors of 2
@@ -21,6 +20,7 @@
 #      eg: num = 13, num-1 = 12 = 2*2*3, num+1 = 14 = 2*7.
 #   b. So which is the easier way to reach half of more even number i.e 12.
 #      And we need to reach 6 from 13
+#
 #
 
 def get_more_neigh_even(odd_num):
@@ -51,16 +51,17 @@ def reach_zero(num):
     temp = num
     extra_cost = 0
 
-    # If number is odd.
+    # If number is odd we have to go to even num, keeping extra_cost in mind.
     if num%2 == 1:
       temp = get_more_neigh_even(num)
       extra_cost = add_op_cost
 
     mul_way_cost = extra_cost + mul_op_cost
     add_way_cost = add_op_cost * (num-temp/2)
-
-    num = temp/2
     cost += min(mul_way_cost, add_way_cost)
+    
+    num = temp/2
+    
   return cost
 
 # Inputs.
