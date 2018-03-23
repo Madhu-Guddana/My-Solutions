c00801a66e53c2a37740fd295f6e745486a8d0d3
Madhu-Guddana <36655676+Madhu-Guddana@users.noreply.github.com>
2018-03-23 09:28:41 +0530

    Update coin_change_problem.py

--- dynamic_programming/coin_change_problem.py
+++ dynamic_programming/coin_change_problem.py
@@ -1,6 +1,7 @@
 # Question:
 # In this problem you will be given a list of coin denominations and a target amount. 
 # Determine the number of ways the target amount can be arrived at using the denominations available.
+# We have unlimited supply of coins.
 
 # Source
 # https://www.hackerrank.com/challenges/coin-change/problem
@@ -10,16 +11,18 @@
 import sys
 
 def getWays(sum_req, coins):
+  coins.sort()
   cost_mat = [[0]*len(coins) for _ in range(sum_req+1)]
   cost_mat[0] = [1]*len(coins) # Sum req=0, all coins has 1 way.
 
   for s in range(1,sum_req+1):
     for c in range(len(coins)):
       sel_ways = unsel_ways = 0
-      if c-1 >=0:
+      if c-1 >=0: # If it is not first coin.
         unsel_ways = cost_mat[s][c-1]
-      if s-coins[c] >=0:
-        sel_ways = cost_mat[s-coins[c]][c]
+      rem_sum = s-coins[c]
+      if rem_sum >=0: # If the value of coin doesn't exceed required sum.
+        sel_ways = cost_mat[rem_sum][c]
       cost_mat[s][c] = unsel_ways +sel_ways
   return cost_mat[-1][-1]
 
