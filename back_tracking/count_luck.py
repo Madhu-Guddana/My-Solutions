e4c0bf7a9e7ebc052f13e9a0ead6065ffc9aa22c
Madhu-Guddana <36655676+Madhu-Guddana@users.noreply.github.com>
2018-03-23 09:19:05 +0530

    Update count_luck.py

--- back_tracking/count_luck.py
+++ back_tracking/count_luck.py
@@ -1,5 +1,17 @@
 # Source: https://www.hackerrank.com/challenges/count-luck/problem
 
+# Question:
+# Consider the forest as an  grid N X M. Each cell is either empty(.) or
+# blocked by a tree (x). A person can move LEFT, RIGHT, UP, and DOWN
+# through empty cells but not via tree cells. His cur cell is marked with
+# the character 'M' , and dest is marked  with '*' . The upper-left corner
+# is indexed as [0][0].
+
+# Assume there is only 1 way possible to reach destination.
+# k is the estimated number of points where path splits.
+# Print "Impressed" if estimation is correct, "Oops!" otherwise.
+
+
 #!/bin/python3
 
 import sys
@@ -7,9 +19,9 @@ import sys
 def get_available_paths(matrix, pos):
   r = pos[0]
   c = pos[1]
-  neigh = [            (r-1, c),
-           (r,   c-1),           (r,   c+1),
-                       (r+1, c)]
+  neigh = [       (r-1, c),
+           (r, c-1),     (r, c+1),
+                  (r+1, c)]
   ret_val = []
   for ele in neigh:
     i,j = ele[0], ele[1]
@@ -34,7 +46,8 @@ def traverse(matrix, pos, count):
   return -1
 
 def countLuck(matrix, k):
-  source =[(i,j) for i in range(len(matrix)) for j in range(len(matrix[i])) if matrix[i][j] == 'M']
+  source =[(i,j) for i in range(len(matrix)) for j in range(len(matrix[i]))
+           if matrix[i][j] == 'M']
   source = source[0]
   ans = traverse(matrix, source, 0)
   if ans == k:
